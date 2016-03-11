8734694e8b63c14ce1ae5bfb850e07176f2b0add
stlouisweb <stlouisweb@gmail.com>
2016-03-10 20:39:09 -0600

    adds mock data to Note component.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,31 @@
+import uuid from "node-uuid";
 import React from "react";
-import Note from "./Note.jsx";
+//import Note from "./Note.jsx";
 
 export default class App extends React.Component {
   render() {
-    return <Note />;
+    const notes = [
+      {
+        id: uuid.v4(),
+        task: "Learn Webpack"
+      },
+      {
+        id: uuid.v4(),
+        task: "Learn React"
+      },
+      {
+        id: uuid.v4(),
+        task: "Do laundry"
+      }
+    ];
+    //return <Note />;
+
+    return (
+      <div>
+        <ul>{notes.map(note =>
+          <li key={note.id}>{note.task}</li>
+        )}</ul>
+      </div>
+    );
   }
 }
